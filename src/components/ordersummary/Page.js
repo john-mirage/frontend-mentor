f87c31893f70c0e63c5851147960cec1b016272f
chaosdynamix <chaosdynamix@posteo.net>
2022-01-04 04:33:23 +0100

    fix(page): fix background url in ordersummary

--- src/components/ordersummary/Page.js
+++ src/components/ordersummary/Page.js
@@ -3,6 +3,8 @@ import Dialog from "@components/ordersummary/Dialog";
 import Order from "@components/ordersummary/Order";
 import heroIllustration from "@assets/ordersummary/illustration-hero.svg";
 import musicIcon from "@assets/ordersummary/icon-music.svg";
+import patternDesktop from "@assets/ordersummary/pattern-background-desktop.svg";
+import patternMobile from "@assets/ordersummary/pattern-background-mobile.svg";
 
 const Container = styled.main`
     width: 100%;
@@ -12,12 +14,12 @@ const Container = styled.main`
     align-items: center;
     padding: 2.5rem;
     background-color: ${props => props.theme.paleBlue};
-    background-image: url("./assets/ordersummary/pattern-background-mobile.svg");
+    background-image: url(${patternMobile.src});
     background-repeat: no-repeat;
     background-size: 100% 50%;
 
     @media screen and (min-width: 532px) {
-        background-image: url("./assets/ordersummary/pattern-background-desktop.svg");
+        background-image: url(${patternDesktop.src});
     }
 `;
 
