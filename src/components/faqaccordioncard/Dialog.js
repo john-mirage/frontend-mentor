9d21dc51cb2774af687185faa3e7779a73f47fec
chaosdynamix <chaosdynamix@posteo.net>
2022-01-04 13:48:29 +0100

    feat(comp): add illustration shadow for faq dialog

--- src/components/faqaccordioncard/Dialog.js
+++ src/components/faqaccordioncard/Dialog.js
@@ -1,25 +1,47 @@
 import Image from "next/image";
 import styled from "styled-components";
 import Accordion from "@components/faqaccordioncard/Accordion";
-import heroIllustration from "@assets/faqaccordioncard/illustration-woman-online-mobile.svg";
+import mobileIllustration from "@assets/faqaccordioncard/illustration-woman-online-mobile.svg";
+import mobilePattern from "@assets/faqaccordioncard/bg-pattern-mobile.svg";
 
 const Container = styled.article`
+    position: relative;
     width: 100%;
     height: auto;
     border-radius: 2.4rem;
-    padding: 2rem;
     background-color: #fff;
     box-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1);
+
+    @media screen and (min-width: 1048px) {
+        width: 100rem;
+        margin-left: auto;
+        margin-right: auto;
+    }
 `;
 
-const ImageContainer = styled.div`
-    display: block;
-    width: 70%;
+const MobileIllustration = styled.div`
+    position: absolute;
+    z-index: 20;
+    top: 0;
+    left: 50%;
+    transform: translate(-50%, -60%);
+    width: 75%;
     height: auto;
-    margin-top: -37%;
-    margin-bottom: 3.2rem;
-    margin-left: auto;
-    margin-right: auto;
+`;
+
+const MobilePattern = styled.div`
+    position: absolute;
+    z-index: 10;
+    top: 0;
+    left: 50%;
+    transform: translate(-50%, -10%);
+    width: 75%;
+    height: auto;
+`;
+
+const Body = styled.div`
+    padding: 2.4rem;
+    margin-top: 30%;
 `;
 
 const Title = styled.h1`
@@ -33,29 +55,36 @@ const Title = styled.h1`
 function Dialog() {
     return (
         <Container>
-            <ImageContainer>
+            <MobileIllustration>
                 <Image
-                    src={heroIllustration}
-                    alt="Illustration of a woman looking at a screen"
+                    src={mobileIllustration}
                     layout="responsive"
                 />
-            </ImageContainer>
-            <Title>FAQ</Title>
-            <Accordion
-                question="How many team members can i invite?"
-                answer="You can invite all the members you want, it's free and unlimited!"
-                accordionId="accordion-1"
-            />
-            <Accordion
-                question="What is the maximum upload file?"
-                answer="No more than 2GB. All files in your account must fit the allotted storage space."
-                accordionId="accordion-2"
-            />
-            <Accordion
-                question="How do i reset my password?"
-                answer="You can reset your password at any time by clicking your avatar and settings / account."
-                accordionId="accordion-3"
-            />
+            </MobileIllustration>
+            <MobilePattern>
+                <Image
+                    src={mobilePattern}
+                    layout="responsive"
+                />
+            </MobilePattern>
+            <Body>
+                <Title>FAQ</Title>
+                <Accordion
+                    question="How many team members can i invite?"
+                    answer="You can invite all the members you want, it's free and unlimited!"
+                    accordionId="accordion-1"
+                />
+                <Accordion
+                    question="What is the maximum upload file?"
+                    answer="No more than 2GB. All files in your account must fit the allotted storage space."
+                    accordionId="accordion-2"
+                />
+                <Accordion
+                    question="How do i reset my password?"
+                    answer="You can reset your password at any time by clicking your avatar and settings / account."
+                    accordionId="accordion-3"
+                />
+            </Body>
         </Container>
     );
 }
