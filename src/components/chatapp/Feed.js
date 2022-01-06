2740eea85ed4921e7c6526a288649bdc346ed198
chaosdynamix <chaosdynamix@posteo.net>
2022-01-06 07:59:20 +0100

    ref(chatapp): simplify structure of feed

--- src/components/chatapp/Feed.js
+++ src/components/chatapp/Feed.js
@@ -3,18 +3,26 @@ import Pictures from "@components/chatapp/Pictures";
 import Proposition from "@components/chatapp/Proposition";
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
+    display: block;
     width: 100%;
     height: auto;
     padding-top: 1.6rem;
 `;
 
+const Section = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: auto;
+    margin-bottom: 2.8rem;
+`;
+
 const Message = styled.p`
+    align-self: ${props => props.right ? "flex-end" : "flex-start"};
     padding: 1rem;
     max-width: 60%;
     height: auto;
-    margin: ${props => props.right ? "0 0 1.6rem auto" : "0 auto 1.6rem 0"};
+    margin-bottom: 1.4rem;
     border-top-left-radius: 1.6rem;
     border-top-right-radius: 1.6rem;
     border-bottom-right-radius: ${props => props.right ? "0.6rem" : "1.6rem"};
@@ -24,19 +32,31 @@ const Message = styled.p`
     font-size: 1.4rem;
     font-weight: 400;
     line-height: 2rem;
+
+    &:last-child {
+        margin-bottom: 0;
+    }
 `;
 
 function Feed() {
     return (
         <Container>
-            <Message>That sounds great. I'd be happy to discuss more.</Message>
-            <Message>Could you send over some pictures of your dog, please?</Message>
-            <Pictures />
-            <Message right>Here are a few pictures. She's an happy girl!</Message>
-            <Message right>Can you make it?</Message>
-            <Message>She looks so happy! The we discussed works. How long shall i take her out for?</Message>
-            <Proposition duration="30 minutes walk" price="$29" inputId="proposition-selector-1" />
-            <Proposition duration="1 hour walk" price="$49" inputId="proposition-selector-2" />
+            <Section>
+                <Message>That sounds great. I'd be happy to discuss more.</Message>
+                <Message>Could you send over some pictures of your dog, please?</Message>
+            </Section>
+            
+            <Section>
+                <Pictures />
+                <Message right>Here are a few pictures. She's an happy girl!</Message>
+                <Message right>Can you make it?</Message>
+            </Section>
+            
+            <Section>
+                <Message>She looks so happy! The we discussed works. How long shall i take her out for?</Message>
+                <Proposition duration="30 minutes walk" price="$29" inputId="proposition-selector-1" />
+                <Proposition duration="1 hour walk" price="$49" inputId="proposition-selector-2" />
+            </Section>
         </Container>
     );
 }
