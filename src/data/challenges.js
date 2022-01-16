ef7bf1f6717a9fc51865a868bfd72f169a2ca044
chaosdynamix <chaosdynamix@posteo.net>
2022-01-16 20:17:15 +0100

    feat(data): add challenges data

---
+++ src/data/challenges.js
@@ -0,0 +1,61 @@
+import orderSummaryImage from "@assets/index/order-summary.jpg";
+import NFTPreviewCardImage from "@assets/index/nft-preview-card.jpg";
+import FAQAccordionCardImage from "@assets/index/faq-accordion-card.jpg";
+import ChatAppCSSIllustrationImage from "@assets/index/chat-app-css-illustration.jpg";
+import FyloDataStorageImage from "@assets/index/fylo-data-storage.jpg";
+import InteractivePricingImage from "@assets/index/interactive-pricing.jpg";
+import LaunchCountdownTimerImage from "@assets/index/launch-countdown-timer.jpg";
+
+const challenges = [
+    {
+        title: "Order Summary Component",
+        description: "A perfect project for newbies who are starting to build confidence with layouts!",
+        link: "/order-summary",
+        image: orderSummaryImage.src,
+        imageAlt:"Order Summary Component design illustration",
+    },
+    {
+        title: "NFT Preview Card Component",
+        description: "This HTML & CSS only challenge is perfect for anyone just starting out or anyone wanting a small project to play around with.",
+        link: "/nft-preview-card",
+        image: NFTPreviewCardImage.src,
+        imageAlt:"NFT Preview Card Component design illustration",
+    },
+    {
+        title: "FAQ Accordion Card Component",
+        description: "In this challenge, you'll be building out an FAQ accordion. This is an extremely common front-end pattern, so it's a great opportunity to get some practice in!",
+        link: "/faq-accordion-card",
+        image: FAQAccordionCardImage.src,
+        imageAlt:"FAQ Accordion Card Component design illustration",
+    },
+    {
+        title: "Chat App CSS Illustration",
+        description: "In this challenge, you'll be building out the entire app illustration from scratch. This will seriously test your CSS skills. So give it a go if you feel confident!",
+        link: "/chat-app-css-illustration",
+        image: ChatAppCSSIllustrationImage.src,
+        imageAlt:"Chat App CSS design illustration",
+    },
+    {
+        title: "Fylo data storage Illustration",
+        description: "This component has some interesting CSS challenges in the design. If you're looking to test your CSS skills, this will be a great project for you!",
+        link: "/fylo-data-storage",
+        image: FyloDataStorageImage.src,
+        imageAlt:"Fylo data storage design illustration",
+    },
+    {
+        title: "Interactive Pricing component Illustration",
+        description: "In this project, you'll build out an interactive pricing component complete with custom range input slider and pricing toggle. A perfect way to test your JS skills!",
+        link: "/interactive-pricing",
+        image: InteractivePricingImage.src,
+        imageAlt:"Interactive Pricing component illustration",
+    },
+    {
+        title: "Launch Countdown Timer component Illustration",
+        description: "This will be a fun one! Your challenge is to build this countdown timer. There are lots of small CSS tests in the design as well. So it should keep you busy!",
+        link: "/launch-countdown-timer",
+        image: LaunchCountdownTimerImage.src,
+        imageAlt:"Launch Countdown Timer component Illustration",
+    },
+];
+
+export default challenges;
