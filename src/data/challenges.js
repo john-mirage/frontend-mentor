d7c69ec01fe1f315e459890e1175a601502b904d
chaosdynamix <chaosdynamix@posteo.net>
2022-01-17 21:02:56 +0100

    feat(index): add ecommerce product page

--- src/data/challenges.js
+++ src/data/challenges.js
@@ -1,10 +1,11 @@
 import orderSummaryImage from "@assets/index/order-summary.jpg";
-import NFTPreviewCardImage from "@assets/index/nft-preview-card.jpg";
-import FAQAccordionCardImage from "@assets/index/faq-accordion-card.jpg";
-import ChatAppCSSIllustrationImage from "@assets/index/chat-app-css-illustration.jpg";
-import FyloDataStorageImage from "@assets/index/fylo-data-storage.jpg";
-import InteractivePricingImage from "@assets/index/interactive-pricing.jpg";
-import LaunchCountdownTimerImage from "@assets/index/launch-countdown-timer.jpg";
+import nftPreviewCardImage from "@assets/index/nft-preview-card.jpg";
+import faqAccordionCardImage from "@assets/index/faq-accordion-card.jpg";
+import chatAppCSSIllustrationImage from "@assets/index/chat-app-css-illustration.jpg";
+import fyloDataStorageImage from "@assets/index/fylo-data-storage.jpg";
+import interactivePricingImage from "@assets/index/interactive-pricing.jpg";
+import launchCountdownTimerImage from "@assets/index/launch-countdown-timer.jpg";
+import ecommerceProductPageImage from "@assets/index/ecommerce-product-page.jpg";
 
 const challenges = [
     {
@@ -18,43 +19,50 @@ const challenges = [
         title: "NFT Preview Card Component",
         description: "This HTML & CSS only challenge is perfect for anyone just starting out or anyone wanting a small project to play around with.",
         link: "/nft-preview-card",
-        image: NFTPreviewCardImage.src,
+        image: nftPreviewCardImage.src,
         imageAlt:"NFT Preview Card Component design illustration",
     },
     {
         title: "FAQ Accordion Card Component",
         description: "In this challenge, you'll be building out an FAQ accordion. This is an extremely common front-end pattern, so it's a great opportunity to get some practice in!",
         link: "/faq-accordion-card",
-        image: FAQAccordionCardImage.src,
+        image: faqAccordionCardImage.src,
         imageAlt:"FAQ Accordion Card Component design illustration",
     },
     {
         title: "Chat App CSS Illustration",
         description: "In this challenge, you'll be building out the entire app illustration from scratch. This will seriously test your CSS skills. So give it a go if you feel confident!",
         link: "/chat-app-css-illustration",
-        image: ChatAppCSSIllustrationImage.src,
+        image: chatAppCSSIllustrationImage.src,
         imageAlt:"Chat App CSS design illustration",
     },
     {
         title: "Fylo data storage Illustration",
         description: "This component has some interesting CSS challenges in the design. If you're looking to test your CSS skills, this will be a great project for you!",
         link: "/fylo-data-storage",
-        image: FyloDataStorageImage.src,
+        image: fyloDataStorageImage.src,
         imageAlt:"Fylo data storage design illustration",
     },
     {
         title: "Interactive Pricing component Illustration",
         description: "In this project, you'll build out an interactive pricing component complete with custom range input slider and pricing toggle. A perfect way to test your JS skills!",
         link: "/interactive-pricing",
-        image: InteractivePricingImage.src,
+        image: interactivePricingImage.src,
         imageAlt:"Interactive Pricing component illustration",
     },
     {
         title: "Launch Countdown Timer component Illustration",
         description: "This will be a fun one! Your challenge is to build this countdown timer. There are lots of small CSS tests in the design as well. So it should keep you busy!",
         link: "/launch-countdown-timer",
-        image: LaunchCountdownTimerImage.src,
+        image: launchCountdownTimerImage.src,
         imageAlt:"Launch Countdown Timer component Illustration",
+    },
+    {
+        title: "E-commerce Product Page Illustration",
+        description: "In this challenge, you'll build a beautiful product page. We'll be putting your JS skills to the test with a lightbox product gallery and cart functionality!",
+        link: "/ecommerce-product-page",
+        image: ecommerceProductPageImage.src,
+        imageAlt:"E-commerce Product Page Illustration",
     },
 ];
 
