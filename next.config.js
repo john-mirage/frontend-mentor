a0093b3b07bc756eb03d312676ccb0e187c6e49b
chaosdynamix <chaosdynamix@posteo.net>
2022-01-25 14:47:00 +0100

    revert(nex): remove webpack conf to see default output

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 module.exports = {
     basePath: "/frontend-mentor",
-    webpack(config) {
-        
-        config.module.rules.push({
-            type: 'asset',
-            resourceQuery: /url/,
-        });
-
-        config.module.rules.push({
-            test: /\.svg$/i,
-            issuer: /\.[jt]sx?$/,
-            use: ['@svgr/webpack'],
-        });
-
-        return config;
-    },
+    
 }
