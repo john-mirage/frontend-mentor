2d4c394f892e8a604358541327feaef5d56fc82e
chaosdynamix <chaosdynamix@posteo.net>
2022-02-10 07:26:30 +0100

    feat(ecommerce): fully animate thumbnails

--- src/components/solution/ecommerce-product-page/thumbnail.js
+++ src/components/solution/ecommerce-product-page/thumbnail.js
@@ -1,39 +1,63 @@
 import styled, { css } from 'styled-components';
+import { motion } from 'framer-motion';
 
 const Container = styled.li`
     position: relative;
     width: 100%;
     height: auto;
-    border-radius: 1rem;
-    overflow: hidden;
+    padding-top: 100%;
     cursor: pointer;
-    ${props => props.isActive && css`
-        outline: 0.2rem solid ${props.theme.color.primary.orange};
-    `}
 `;
 
-const Image = styled.img`
-    width: 100%;
-    height: auto;
-`;
-
-const Overlay = styled.div`
+const layer = css`
     position: absolute;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
-    transition: background-color 300ms;
-    ${props => props.isActive && css`
-        background-color: rgba(255, 255, 255, 0.5);
-    `}
+`;
+
+const Background = styled(motion.div)`
+    ${layer}
+    z-index: 50;
+    background-color: ${props => props.theme.color.primary.orange};
+    border-radius: 1.2rem;
+`;
+
+const Image = styled.img`
+    ${layer}
+    z-index: 60;
+    object-fit: cover;
+    border-radius: 1rem;
+`;
+
+const Overlay = styled(motion.div)`
+    ${layer}
+    z-index: 70;
+    border-radius: 1rem;
+    background-color: rgba(255, 255, 255, 0.5);
 `;
 
 function Thumbnail({ className, thumbnail, action, isActive }) {
     return (
-        <Container className={className} onClick={action} isActive={isActive}>
-            <Image src={thumbnail.src} alt={thumbnail.alt} />
-            <Overlay isActive={isActive} />
+        <Container
+            className={className}
+            onClick={action}
+        >
+            <Background
+                initial={false}
+                animate={{ scale: isActive ? 1.05 : 0.8 }}
+                transition={{ type: "spring", duration: 0.3 }}
+            />
+            <Image
+                src={thumbnail.src}
+                alt={thumbnail.alt}
+            />
+            <Overlay
+                initial={false}
+                animate={{ opacity: isActive ? 1 : 0 }}
+                transition={{ type: "spring", duration: 0.3 }}
+            />
         </Container>
     );
 }
