c4276e6e838d4ff997aef5717aa6921203eaa472
john-mirage <28307090+john-mirage@users.noreply.github.com>
2023-05-18 13:16:09 +0200

    feat: setup tests

--- apps/nft-preview-card-component/vite.config.ts
+++ apps/nft-preview-card-component/vite.config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
       dir: "../../node_modules/.vitest",
     },
     environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
     include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
   },
 });
