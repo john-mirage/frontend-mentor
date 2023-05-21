6aa4f47dca3edb4a3d4381bc64558831225e02aa
john-mirage <28307090+john-mirage@users.noreply.github.com>
2023-05-21 16:14:58 +0200

    feat: remove skeleton projects and add real project

--- apps/age-calculator-app/vite.config.ts
+++ apps/age-calculator-app/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  cacheDir: "../../node_modules/.vite/nft-preview-card-component",
+  cacheDir: "../../node_modules/.vite/age-calculator-app",
 
   server: {
     port: 4200,
