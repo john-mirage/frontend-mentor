bc069d4d1ca7b5943bc5a88f99eee4c72141830e
john-mirage <28307090+john-mirage@users.noreply.github.com>
2023-05-28 18:19:59 +0200

    fix(age-calculator-app): fix button accessibility

--- apps/age-calculator-app/src/components/button/button.tsx
+++ apps/age-calculator-app/src/components/button/button.tsx
@@ -7,6 +7,7 @@ export function Button(props: ButtonProps) {
   return (
     <button
       type="submit"
+      aria-label="Confirm"
       className="relative z-20 flex justify-center items-center w-64 h-64 rounded-full bg-purple transition-colors focus-visible:outline-offset-8 focus-visible:outline-purple hover-device:hover:bg-off-black @2xl:w-96 @2xl:h-96"
     >
       <IconArrow className="w-24 h-24 @2xl:w-44 @2xl:h-44" />
