97427b7dc345fd0b38f2bf71fa26ef12bf7deb08
john-mirage <28307090+john-mirage@users.noreply.github.com>
2023-06-03 08:18:22 +0200

    fix(bmi-calculator): move main landmark

--- apps/bmi-calculator/src/components/app/app.tsx
+++ apps/bmi-calculator/src/components/app/app.tsx
@@ -7,16 +7,14 @@ export function App() {
   return (
     <div className="relative pb-96 lg:pb-128">
       <div className="absolute z-10 top-0 right-0 w-full h-640 bg-gradient-to-br from-hero-from to-hero-to rounded-b-35 lg:right-1/2 lg:translate-x-[300px] lg:w-960 lg:h-737"></div>
-      <div className="relative z-50 container">
+      <main className="relative z-50 container">
         <div className="lg:flex lg:flex-row lg:justify-between lg:items-start">
           <Hero className="mb-48 lg:mb-0" />
           <Bmi className="mb-72 md:mb-96 lg:mt-168 lg:mb-0 lg:w-564" />
         </div>
-        <main>
-          <AdviceList classname="mb-128 md:mb-156 lg:mb-216" />
-          <LimitationList />
-        </main>
-      </div>
+        <AdviceList classname="mb-128 md:mb-156 lg:mb-216" />
+        <LimitationList />
+      </main>
     </div>
   );
 }
